ec93fb41d53d4fa22c5293cab7747abf8438fa5d
Gnanvi Janardhan <gnanvigedam1@gmail.com>
2026-05-29 14:31:42 +0530

    fix url routing and frontend dist

--- config/urls.py
+++ config/urls.py
@@ -15,21 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('apps.sources.urls')),
-    path('api/review/', include('apps.review.urls')),
-]
-
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.sources.urls')),
     path('api/review/', include('apps.review.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
+
+
